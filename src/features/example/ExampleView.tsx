9a1663c00bef0218cc88d43ecd60816bf5b80814
Osahon Usuanlele <usuanlele.osahon@gmail.com>
2026-01-12 17:20:42 +0100

    chore: prepare repo for github template

--- src/features/example/ExampleView.tsx
+++
@@ -1,9 +0,0 @@
-import { useExamples } from "./example.hooks";
-
-export const ExampleView = () => {
-  const { data, isLoading } = useExamples();
-
-  if (isLoading) return <p>Loading...</p>;
-
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
-};
--- src/features/example/example.hooks.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getExamples } from "./example.api";
-
-export const useExamples = () =>
-  useQuery({
-    queryKey: ["examples"],
-    queryFn: getExamples,
-  });
