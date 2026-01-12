70d3bda88f6e4d8e29ebd8d496ae1bb700751040
Osahon Usuanlele <usuanlele.osahon@gmail.com>
2026-01-12 17:14:34 +0100

    feat: setup shadcn ui system

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+import { toast } from "sonner";
+
 const App = () => {
+  // toast.success("Tailwind v4 is working 🚀");
+  toast("Welcome to the Osahon React Starter Template!");
+
   return (
     <div className="p-6 text-red-500 font-semibold">
       Tailwind v4 is working 🚀
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Providers } from "./app/providers";
+import { Toaster } from "./components/ui/sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Providers>
       <App />
+      <Toaster />
     </Providers>
   </React.StrictMode>
 );
